96262a011871fcce34b6332592f7372c7b7de691
baanis <76463514+baanis@users.noreply.github.com>
2021-08-06 16:16:53 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function setup() {
+    canvas = createCanvas(550, 550);
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
